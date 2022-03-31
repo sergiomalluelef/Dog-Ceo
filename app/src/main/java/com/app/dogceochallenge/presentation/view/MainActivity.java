896042bbadbaeb7b0bda1fed3ffe8764f47ede70
Sergio <sergio.m.alluelef@ejemplo.com>
2022-03-30 21:45:25 -0300

    Se agrega buscador.

--- app/src/main/java/com/app/dogceochallenge/presentation/view/MainActivity.java
+++ app/src/main/java/com/app/dogceochallenge/presentation/view/MainActivity.java
@@ -1,11 +1,16 @@
 package com.app.dogceochallenge.presentation.view;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
 import androidx.appcompat.widget.Toolbar;
 import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 
+import com.app.dogceochallenge.R;
 import com.app.dogceochallenge.databinding.ActivityMainBinding;
 import com.app.dogceochallenge.presentation.view.adapter.DogsAdapter;
 import com.app.dogceochallenge.presentation.viewmodel.DogsListViewModel;
@@ -15,9 +20,11 @@ import com.app.dogceochallenge.presentation.viewmodel.DogsListViewModel;
  * Activity principal encargado de mostrar lista de razas y subrazas.
  */
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
     private ActivityMainBinding binding;
     private DogsListViewModel viewModel;
     private DogsAdapter adapter;
+    private SearchView searchView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,10 +57,45 @@ public class MainActivity extends AppCompatActivity {
         });
 
         binding.refresh.setOnRefreshListener(() -> {
+            resetSearchView();
             viewModel.getListDogs();
         });
 
         viewModel.getListDogs();
 
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_bar, menu);
+        MenuItem searchItem = menu.findItem(R.id.search);
+        searchView = (SearchView) searchItem.getActionView();
+        searchView.setQueryHint("Buscar");
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                Log.d(TAG, "onQueryTextChange: " + newText);
+                adapter.getFilter().filter(newText);
+                return false;
+            }
+        });
+        searchView.setIconified(false);
+        return true;
+    }
+
+    /**
+     * Restablece el buscador
+     */
+    public void resetSearchView() {
+        if(searchView != null){
+            searchView.setQuery("", false);
+        }
+    }
+
+
 }
--- app/src/main/java/com/app/dogceochallenge/presentation/view/adapter/DogsAdapter.java
+++ app/src/main/java/com/app/dogceochallenge/presentation/view/adapter/DogsAdapter.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Filter;
+import android.widget.Filterable;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -14,16 +16,19 @@ import com.app.dogceochallenge.presentation.view.viewholder.HolderDog;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 /**
  * Created by Sergio Muñoz
  * on 29-03-2022.
  * Encargado de dibujar lista de razas y subrazas disponibles.
  */
-public class DogsAdapter extends RecyclerView.Adapter{
+public class DogsAdapter extends RecyclerView.Adapter implements Filterable {
     private final Context context;
     private OnClick onClickListener;
     private final List<DogModel> items;
+    private List<DogModel> itemsFilter;
+    private Filter mFilter;
     public DogsAdapter(Context context) {
         this.context = context;
         this.items = new ArrayList<>();
@@ -70,6 +75,56 @@ public class DogsAdapter extends RecyclerView.Adapter{
 
     public void setOnClickListener(OnClick onClickListener) {
         this.onClickListener = onClickListener;
+    }
+
+    @Override
+    public Filter getFilter() {
+        if (mFilter == null) {
+            mFilter = new Filter() {
+                @Override
+                protected FilterResults performFiltering(CharSequence constraint) {
+                    FilterResults results = new FilterResults();
+                    List<DogModel> FilteredArrList = new ArrayList<>();
+                    if (itemsFilter == null || itemsFilter.isEmpty()) {
+                        itemsFilter = new ArrayList<>(items);
+                    }
+                    if (constraint == null) {
+                        results.count = itemsFilter.size();
+                        results.values = itemsFilter;
+                    } else {
+                        Locale locale = Locale.getDefault();
+                        String sConstraint = constraint.toString().trim().toLowerCase(locale);
+                        if (sConstraint.length() == 0) {
+                            results.count = itemsFilter.size();
+                            results.values = itemsFilter;
+                        } else {
+                            for (int i = 0; i < itemsFilter.size(); i++) {
+                                DogModel model = itemsFilter.get(i);
+                                String name = model.getName();
+                                String subName = model.getSubName();
+                                if (name.toLowerCase(locale).contains(sConstraint)
+                                        || subName.toLowerCase(locale).contains(sConstraint)
+                                ) {
+                                    FilteredArrList.add(model);
+                                }
+                            }
+                            results.count = FilteredArrList.size();
+                            results.values = FilteredArrList;
+
+                        }
+                    }
+                    return results;
+                }
+
+                @Override
+                protected void publishResults(CharSequence constraint, FilterResults results) {
+                    items.clear();
+                    items.addAll((ArrayList<DogModel>) results.values);
+                    notifyDataSetChanged();
+                }
+            };
+        }
+        return mFilter;
     }
 
     public interface OnClick {
